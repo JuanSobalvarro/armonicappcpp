5e2b0df2f99f5b036c99ad23933db606bde32f6e
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-07-14 02:58:50 -0600

    Working until 3 am xd

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,155 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    this->setGeometry(0, 0, 720, 480);
+
+    createActions();
+    createMenus();
+    createStatusBar();
+    createBarChart();
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+    delete inputDevices;
+    delete outputDevices;
+}
+
+void MainWindow::populateAudioDeviceMenu()
+{
+    // Create an action group for exclusive selection
+    QActionGroup *deviceActionGroup = new QActionGroup(this);
+    deviceActionGroup->setExclusive(true);
+
+    // Initialize the QList objects
+    inputDevices = new QList<QAudioDevice>;
+    outputDevices = new QList<QAudioDevice>;
+
+    // Retrieve and add audio input/output devices
+    *inputDevices = QMediaDevices::audioInputs();
+    *outputDevices = QMediaDevices::audioOutputs();
+
+    for (const QAudioDevice &device : *inputDevices)
+    {
+        QAction *deviceAction = inputDeviceMenu->addAction(device.description());
+        deviceAction->setCheckable(true);
+        deviceAction->setActionGroup(deviceActionGroup);
+        connect(deviceAction, &QAction::triggered, this, [this, device]() {
+            selectDevice(device.description());
+        });
+    }
+
+    for (const QAudioDevice &device : *outputDevices)
+    {
+        QAction *deviceAction = outputDeviceMenu->addAction(device.description());
+        deviceAction->setCheckable(true);
+        deviceAction->setActionGroup(deviceActionGroup);
+        connect(deviceAction, &QAction::triggered, this, [this, device]() {
+            selectDevice(device.description());
+        });
+    }
+}
+
+void MainWindow::createActions()
+{
+    testAct = new QAction(QIcon::fromTheme(QIcon::ThemeIcon::DialogInformation),
+                          tr("&KKCK"), this);
+    testAct->setWhatsThis("TEST WHATS THIS");
+    // connect with a lambda function since testAction needs parameters
+    connect(testAct, &QAction::triggered, this, [this]()
+            {this->testAction("KKCK");
+            });
+}
+
+void MainWindow::createMenus()
+{
+    menuBar = new QMenuBar(this);
+
+    deviceMenu = new QMenu("Device", this);
+    inputDeviceMenu = deviceMenu->addMenu("Input Devices");
+    outputDeviceMenu = deviceMenu->addMenu("Output Devices");
+    menuBar->addMenu(deviceMenu);
+
+    populateAudioDeviceMenu();
+
+    setMenuBar(menuBar);
+}
+
+void MainWindow::createStatusBar()
+{
+    statusBar = new QStatusBar(this);
+    statusBar->setStyleSheet("QStatusBar { border-top: 1px solid black; }");
+
+    statusMsg = new QLabel();
+
+    statusBar->addWidget(statusMsg);
+
+    setStatusBar(statusBar);
+}
+
+void MainWindow::updateStatusBar(const QString &message)
+{
+    statusMsg->setText(message);
+}
+
+void MainWindow::createBarChart()
+{
+    // DATA
+
+    // Categories
+    QStringList categories;
+    categories << "uwu" << "nya" << "nichan";
+
+    // Values
+    QBarSet *set = new QBarSet("KKCK data");
+    *set << 69 << 420 << 666;
+
+    // The sets should be store in a bar series
+    QBarSeries *series = new QBarSeries();
+    series->append(set);
+
+    // Create chart and add the series
+    barChart = new QChart();
+    barChart->addSeries(series);
+    barChart->setTitle("Frequencies detected");
+
+    // Set up x axis
+    QBarCategoryAxis *xAxis = new QBarCategoryAxis();
+    xAxis->append(categories);
+
+    barChart->addAxis(xAxis, Qt::AlignBottom);
+    series->attachAxis(xAxis);
+
+    // Set up y axis(values)
+    QBarCategoryAxis *yAxis = new QBarCategoryAxis();
+    yAxis->setTitleText("Frequencies");
+    barChart->addAxis(yAxis, Qt::AlignLeft);
+    series->attachAxis(yAxis);
+
+    // Create chart view and set
+    QChartView *chartView = new QChartView(barChart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+    chartView->chart()->setTheme(QChart::ChartThemeDark);
+
+    setCentralWidget(chartView);
+}
+
+// Actions slots
+
+void MainWindow::testAction(std::string activatedBy)
+{
+    QMessageBox msgBox;
+    QString str = "Activated by: " + QString::fromStdString(activatedBy);
+    msgBox.setText(str);
+    msgBox.setStandardButtons(QMessageBox::Ok);
+    msgBox.exec();
+}
+
+void MainWindow::selectDevice(const QString &deviceName)
+{
+    const QString str = "Selected device: " + deviceName;
+
+    updateStatusBar(str);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,31 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QMenu>
+#include <QMenuBar>
+#include <QStatusBar>
+#include <QActionGroup>
+#include <QAction>
+#include <QtMultimedia/QAudioDevice>
+#include <QtMultimedia/QMediaDevices>
+#include <QList>
+#include <QLabel>
+#include <QGraphicsWidget>
+
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QBarCategoryAxis>
+#include <QtCharts/QChartView>
+#include <QtCharts/QValueAxis>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
+class QAction;
+class QMenu;
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -17,7 +37,42 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void testAction(std::string activatedBy);
+
+    void selectDevice(const QString &deviceName);
+
 private:
     Ui::MainWindow *ui;
+
+    void createActions();
+    void createMenus();
+    void createStatusBar();
+    void updateStatusBar(const QString &message);
+
+    void populateAudioDeviceMenu();
+
+    void createBarChart();
+
+    // Bar Chart
+    QChart *barChart;
+
+    // Audio devices
+    QList<QAudioDevice> *inputDevices;
+    QList<QAudioDevice> *outputDevices;
+
+    QStatusBar *statusBar;
+    QLabel *statusMsg;
+
+    // Menus
+    QMenuBar *menuBar;
+
+    QMenu *testMenu;
+    QMenu *deviceMenu;
+    QMenu *inputDeviceMenu;
+    QMenu *outputDeviceMenu;
+
+    // Actions
+    QAction *testAct;
 };
 #endif // MAINWINDOW_H
